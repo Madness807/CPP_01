ce9a0353455650ecd19042c7692e54ecb01b17a5
Madness807 <jonathan.terrettaz@gmail.com>
2024-01-18 13:35:56 +0100

    nms

--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,7 +6,33 @@
 /*   By: joterrett <joterrett@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:08:12 by joterrett         #+#    #+#             */
-/*   Updated: 2024/01/18 13:08:13 by joterrett        ###   ########.fr       */
+/*   Updated: 2024/01/18 13:35:44 by joterrett        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef [NOM_CLASSE]_HPP
+#define [NOM_CLASSE]_HPP
+
+#include <iostream>
+#include <string>
+// Inclure d'autres bibliothèques nécessaires
+
+class [NomClasse] {
+    public:
+        // Constructeurs
+        [NomClasse]();
+        [NomClasse](const [NomClasse] &source);
+
+        // Destructeur
+        ~[NomClasse]();
+
+        // Opérateur d'affectation
+        [NomClasse] &operator=(const [NomClasse] &source);
+
+        // Autres méthodes publiques
+
+    private:
+        // Attributs privés
+};
+
+#endif /* ![NOM_CLASSE]_HPP */
